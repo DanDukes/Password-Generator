08857400522da826dcf8f5cebac89eee9c22efc6
Dan D <degeare28@gmail.com>
2019-11-21 20:18:08 -0700

    finished js logic

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //Define Individual arrays per Character Set
 //set chars as strings and use split, less typing this way
-var letters = "abcdefghijklmnopqrstuvwxyz"; // string of letters
+var lettersLower = "abcdefghijklmnopqrstuvwxyz"; // string of letters
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789"; //string of numbers
 var specials = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"; //string of special chars, note the \ to escape the double quote
 
 //create the arrays that hold the Character Sets
-var lettersLower = letters.split(""); //lowercase array
-var lettersUpper = letters.toUpperCase().split(""); //uppercase array
-var numbers = numbers.split(""); //turn numbers into array
-var specials = specials.split(""); //turn special chars into array
+//var lettersLower = letters.split(""); //lowercase array
+//var lettersUpper = letters.toUpperCase().split(""); //uppercase array
+//var numbers = numbers.split(""); //turn numbers into array
+//var specials = specials.split(""); //turn special chars into array
 
 //Log arrays to check for accuracy
 console.log(lettersLower); //log lowercase array
@@ -24,7 +25,6 @@ function ValidateCharSelects() {
   const warning = document.querySelector(".warning"); //defines warning as any element in the DOM with the .warning class
   var generate = document.getElementById("generate"); //defines generate as the element in the DOM with the unique generate ID
   var numberOfCheckedItems = 0; //set number of checked items to 0 before every loop to avoid accidental addition
-
   //for if loop that is called whenever a box is manipulated, loops through all the items in our Nodelist named checkboxes and counts the checks
   for (var i = 0; i < checkboxes.length; i++) {
     //full loop through checkboxes, the index i will always be less than the length of the due to starting at 0
@@ -35,28 +35,67 @@ function ValidateCharSelects() {
     warning.classList.add("show"); //add the .show class to elements with the .warning class. .warning class is styled so that it is only visible when it has the .show class as well
     warning.classList.remove("close"); //remove the close class from elements with the .warning class, this ensures that warning will toggle
     generate.setAttribute("disabled", ""); //add the disabled attribute, shuts off the generate password button
-    var runGenerator = false; //setting variable for later, main PW generation loop will only run if this is set to true, to avoid folks getting around the disabled button
   } else {
     warning.classList.add("close"); //all of this is just the inverse of the above, to set states the other way if there ARE more than 2 boxes checked
     warning.classList.remove("show");
     generate.removeAttribute("disabled", "");
-    var runGenerator = true;
   }
 }
-
-//Set up the Correct Character Array to Generate from based on User Preferences
-var passPool = []; //empty array to start with, this is what we will add on to
-
 // Length Slider
-var slider = document.getElementById("myRange");
-var length = document.getElementById("length");
+var slider = document.getElementById("length");
+var length = document.getElementById("value");
 length.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function passLength() {
   length.innerHTML = this.value;
 };
 
-//if lower = true {
-//  charSet  = charset + lower
-//}
+//Main PW Generation Function
+
+//var clipboard = new Clipboard(".copy");
+var lowercaseSelect = document.querySelector('input[value="lettersLower"]'),
+  uppercaseSelect = document.querySelector('input[value="lettersUpper"]'),
+  numbersSelect = document.querySelector('input[value="numbers"]'),
+  specialsSelect = document.querySelector('input[value="specials"]'),
+  passwordBox = document.getElementById("passBox"),
+  generateButton = document.getElementById("generate"),
+  copyButton = document.getElementById("copy"),
+  lengthInput = document.getElementById("length"),
+  passLength,
+  userPassword,
+  passwordPool;
+
+function GeneratePassword() {
+  userPassword = "";
+  passwordPool = "";
+  if (lowercaseSelect.checked) {
+    passwordPool += lettersLower;
+  }
+  if (uppercaseSelect.checked) {
+    passwordPool += lettersUpper;
+  }
+  if (numbersSelect.checked) {
+    passwordPool += numbers;
+  }
+  if (specialsSelect.checked) {
+    passwordPool += specials;
+  }
+  console.log(passwordPool);
+
+  passlength = Number(lengthInput.value);
+
+  for (let i = 0; i < passlength; i++) {
+    userPassword += passwordPool.charAt(
+      Math.floor(Math.random() * passwordPool.length)
+    );
+  }
+  passwordBox.innerHTML = userPassword;
+}
+
+function copyClip() {
+  var copyText = document.getElementById("passBox");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  document.execCommand("copy");
+}
